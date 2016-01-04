35c0056850f4a199dada0198aa73865c55409380
Achim Friedland <github@ahzf.de>
2016-01-04 05:02:23 +0100

    HTTP logger fixes

--- WWCP_OCPI_2_0/Entities/Location/Location_Id.cs
+++ WWCP_OCPI_2_0/Entities/Location/Location_Id.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2014-2015 GraphDefined GmbH
+ * Copyright (c) 2014-2016 GraphDefined GmbH
  * This file is part of WWCP Core <https://github.com/GraphDefined/WWCP_Core>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
