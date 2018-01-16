580a7a10ac5636e3b8b2dfc18358af36714b8ebb
Achim Friedland <github@ahzf.de>
2018-01-16 20:46:31 +0100

    Fixed typo

--- WWCP_OCPIv2.1/DataTypes/RegularHours.cs
+++ WWCP_OCPIv2.1/DataTypes/RegularHours.cs
@@ -181,7 +181,7 @@ namespace org.GraphDefined.WWCP.OCPIv2_1
         #region (override) ToString()
 
         /// <summary>
-        /// Return a string representation of this object.
+        /// Return a text representation of this object.
         /// </summary>
         public override String ToString()
         {
