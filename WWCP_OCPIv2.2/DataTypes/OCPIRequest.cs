961c476a624973cfdff1f7770bb2e15bee0480d4
Achim Friedland <achim.friedland@graphdefined.com>
2020-10-09 01:51:03 +0200

    Implement some missing JSON I/O

---
+++ WWCP_OCPIv2.2/DataTypes/OCPIRequest.cs
@@ -0,0 +1,229 @@
+﻿/*
+ * Copyright (c) 2015-2020 GraphDefined GmbH
+ * This file is part of WWCP OCPI <https://github.com/OpenChargingCloud/WWCP_OCPI>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json.Linq;
+
+using org.GraphDefined.Vanaheimr.Illias;
+using org.GraphDefined.Vanaheimr.Hermod;
+using org.GraphDefined.Vanaheimr.Hermod.DNS;
+using org.GraphDefined.Vanaheimr.Hermod.HTTP;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace cloud.charging.open.protocols.OCPIv2_2.HTTP
+{
+
+
+
+
+    /// <summary>
+    /// A HTTP delegate.
+    /// </summary>
+    /// <param name="Request">The HTTP request.</param>
+    /// <returns>A HTTP response task.</returns>
+    public delegate Task<HTTPResponse> OCPIRequestDelegate(OCPIRequest Request);
+
+
+    /// <summary>
+    /// Extention methods for OCPI requests.
+    /// </summary>
+    public static class OCPIRequestExtentions
+    {
+
+        #region AddOCPIMethod(HTTPServer, Hostname, HTTPMethod, URLTemplate,  HTTPContentType = null, URLAuthentication = false, HTTPMethodAuthentication = false, ContentTypeAuthentication = false, HTTPDelegate = null)
+
+        /// <summary>
+        /// Add a method callback for the given URL template.
+        /// </summary>
+        /// <param name="HTTPServer">The HTTP server.</param>
+        /// <param name="Hostname">The HTTP hostname.</param>
+        /// <param name="HTTPMethod">The HTTP method.</param>
+        /// <param name="URLTemplate">The URL template.</param>
+        /// <param name="HTTPContentType">The HTTP content type.</param>
+        /// <param name="URLAuthentication">Whether this method needs explicit uri authentication or not.</param>
+        /// <param name="HTTPMethodAuthentication">Whether this method needs explicit HTTP method authentication or not.</param>
+        /// <param name="ContentTypeAuthentication">Whether this method needs explicit HTTP content type authentication or not.</param>
+        /// <param name="HTTPRequestLogger">A HTTP request logger.</param>
+        /// <param name="HTTPResponseLogger">A HTTP response logger.</param>
+        /// <param name="DefaultErrorHandler">The default error handler.</param>
+        /// <param name="OCPIRequest">The method to call.</param>
+        public static void AddOCPIMethod(this HTTPServer         HTTPServer,
+                                         HTTPHostname            Hostname,
+                                         HTTPMethod              HTTPMethod,
+                                         HTTPPath                URLTemplate,
+                                         HTTPContentType         HTTPContentType             = null,
+                                         HTTPAuthentication      URLAuthentication           = null,
+                                         HTTPAuthentication      HTTPMethodAuthentication    = null,
+                                         HTTPAuthentication      ContentTypeAuthentication   = null,
+                                         HTTPRequestLogHandler   HTTPRequestLogger           = null,
+                                         HTTPResponseLogHandler  HTTPResponseLogger          = null,
+                                         HTTPDelegate            DefaultErrorHandler         = null,
+                                         OCPIRequestDelegate     OCPIRequest                 = null,
+                                         URLReplacement          AllowReplacement            = URLReplacement.Fail)
+
+        {
+
+            HTTPServer.AddMethodCallback(Hostname,
+                                         HTTPMethod,
+                                         URLTemplate,
+                                         HTTPContentType,
+                                         URLAuthentication,
+                                         HTTPMethodAuthentication,
+                                         ContentTypeAuthentication,
+                                         HTTPRequestLogger,
+                                         HTTPResponseLogger,
+                                         DefaultErrorHandler,
+                                         httpRequest => OCPIRequest(HTTP.OCPIRequest.Parse(httpRequest)),
+                                         AllowReplacement);
+
+        }
+
+        #endregion
+
+    }
+
+
+    /// <summary>
+    /// An OCPI HTTP request.
+    /// </summary>
+    public class OCPIRequest // : HTTPRequest
+    {
+
+        public readonly struct DateAndPaginationFilters
+        {
+
+            public DateTime?  From     { get; }
+            public DateTime?  To       { get; }
+            public UInt64?    Offset   { get; }
+            public UInt64?    Limit    { get; }
+
+
+            public DateAndPaginationFilters(DateTime?  From,
+                                            DateTime?  To,
+                                            UInt64?    Offset,
+                                            UInt64?    Limit)
+            {
+
+                this.From    = From;
+                this.To      = To;
+                this.Offset  = Offset;
+                this.Limit   = Limit;
+
+            }
+
+        }
+
+
+        #region Properties
+
+        public HTTPRequest      HTTPRequest         { get; }
+
+        public Request_Id?      RequestId           { get; }
+        public Correlation_Id?  CorrelationId       { get; }
+        public CountryCode?     ToCountryCode       { get; }
+        public Party_Id?        ToPartyId           { get; }
+        public CountryCode?     FromCountryCode     { get; }
+        public Party_Id?        FromPartyId         { get; }
+
+        public AccessToken?     AccessToken         { get; }
+
+
+        /// <summary>
+        /// The parsed URL parameters of the best matching URL template.
+        /// Set by the HTTP server.
+        /// </summary>
+        public String[]     ParsedURLParameters
+            => HTTPRequest.ParsedURLParameters;
+
+        /// <summary>
+        /// The HTTP query string.
+        /// </summary>
+        public QueryString      QueryString
+            => HTTPRequest?.QueryString;
+
+        #endregion
+
+        protected OCPIRequest(HTTPRequest Request)
+        {
+
+            this.HTTPRequest      = Request ?? throw new ArgumentNullException(nameof(HTTPRequest), "The given HTTP request must not be null!");
+
+            this.RequestId        = Request.TryParseHeaderField<Request_Id>    ("X-Request-ID",           Request_Id.    TryParse);
+            this.CorrelationId    = Request.TryParseHeaderField<Correlation_Id>("X-Correlation-ID",       Correlation_Id.TryParse);
+            this.ToCountryCode    = Request.TryParseHeaderField<CountryCode>   ("OCPI-to-country-code",   CountryCode.   TryParse);
+            this.ToPartyId        = Request.TryParseHeaderField<Party_Id>      ("OCPI-to-party-id",       Party_Id.      TryParse);
+            this.FromCountryCode  = Request.TryParseHeaderField<CountryCode>   ("OCPI-from-country-code", CountryCode.   TryParse);
+            this.FromPartyId      = Request.TryParseHeaderField<Party_Id>      ("OCPI-from-party-id",     Party_Id.      TryParse);
+
+
+            if      (Request.Authorization is HTTPTokenAuthentication TokenAuth &&
+                OCPIv2_2.AccessToken.TryParse(StringExtensions.FromBase64(TokenAuth.Token), out AccessToken accessToken))
+            {
+                this.AccessToken = accessToken;
+            }
+
+            else if (Request.Authorization is HTTPBasicAuthentication BasicAuth &&
+                OCPIv2_2.AccessToken.TryParse(BasicAuth.Username, out accessToken))
+            {
+                this.AccessToken = accessToken;
+            }
+
+
+
+            // Set("X-Request-ID", RequestId).
+            // Set("X-Correlation-ID", CorrelationId);
+
+
+        }
+
+        public DateAndPaginationFilters GetDateAndPaginationFilters()
+
+            => new DateAndPaginationFilters(HTTPRequest.QueryString.GetDateTime("date_from"),
+                                            HTTPRequest.QueryString.GetDateTime("date_to"),
+                                            HTTPRequest.QueryString.GetUInt64  ("offset"),
+                                            HTTPRequest.QueryString.GetUInt64  ("limit"));
+
+
+
+
+        public Boolean TryParseJObjectRequestBody(out JObject       JSON,
+                                                  out HTTPResponse  HTTPResponse,
+                                                  Boolean           AllowEmptyHTTPBody = false,
+                                                  String            JSONLDContext      = null)
+
+            => HTTPRequest.TryParseJObjectRequestBody(out JSON,
+                                                      out HTTPResponse,
+                                                      AllowEmptyHTTPBody,
+                                                      JSONLDContext);
+
+
+
+        public static OCPIRequest Parse(HTTPRequest HTTPRequest)
+
+            => new OCPIRequest(HTTPRequest);
+
+
+    }
+
+}
---
+++ WWCP_OCPIv2.2/DataTypes/Simple/Correlation_Id.cs
@@ -0,0 +1,342 @@
+﻿/*
+ * Copyright (c) 2015-2020 GraphDefined GmbH
+ * This file is part of WWCP OCPI <https://github.com/OpenChargingCloud/WWCP_OCPI>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+
+using org.GraphDefined.Vanaheimr.Illias;
+
+#endregion
+
+namespace cloud.charging.open.protocols.OCPIv2_2
+{
+
+    /// <summary>
+    /// The unique identification of a HTTP correlation.
+    /// </summary>
+    public struct Correlation_Id : IId<Correlation_Id>
+    {
+
+        #region Data
+
+        /// <summary>
+        /// The internal identification.
+        /// </summary>
+        private readonly String InternalId;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Indicates whether this identification is null or empty.
+        /// </summary>
+        public Boolean IsNullOrEmpty
+
+            => InternalId.IsNullOrEmpty();
+
+        /// <summary>
+        /// The length of the correlation identification.
+        /// </summary>
+        public UInt64 Length
+
+            => (UInt64) (InternalId?.Length ?? 0);
+
+        #endregion
+
+        #region Constructor(s)
+
+        /// <summary>
+        /// Create a new HTTP correlation identification based on the given string.
+        /// </summary>
+        /// <param name="String">The string representation of the HTTP correlation identification.</param>
+        private Correlation_Id(String String)
+        {
+            this.InternalId  = String;
+        }
+
+        #endregion
+
+
+        #region (static) Parse   (Text)
+
+        /// <summary>
+        /// Parse the given string as a HTTP correlation identification.
+        /// </summary>
+        /// <param name="Text">A text representation of a correlation identification.</param>
+        public static Correlation_Id Parse(String Text)
+        {
+
+            if (TryParse(Text, out Correlation_Id locationId))
+                return locationId;
+
+            if (Text.IsNullOrEmpty())
+                throw new ArgumentNullException(nameof(Text), "The given text representation of a correlation identification must not be null or empty!");
+
+            throw new ArgumentException("The given text representation of a correlation identification is invalid!", nameof(Text));
+
+        }
+
+        #endregion
+
+        #region (static) TryParse(Text)
+
+        /// <summary>
+        /// Try to parse the given text as a HTTP correlation identification.
+        /// </summary>
+        /// <param name="Text">A text representation of a correlation identification.</param>
+        public static Correlation_Id? TryParse(String Text)
+        {
+
+            if (TryParse(Text, out Correlation_Id locationId))
+                return locationId;
+
+            return null;
+
+        }
+
+        #endregion
+
+        #region (static) TryParse(Text, out CorrelationId)
+
+        /// <summary>
+        /// Try to parse the given text as a HTTP correlation identification.
+        /// </summary>
+        /// <param name="Text">A text representation of a correlation identification.</param>
+        /// <param name="CorrelationId">The parsed correlation identification.</param>
+        public static Boolean TryParse(String Text, out Correlation_Id CorrelationId)
+        {
+
+            if (Text.IsNotNullOrEmpty())
+            {
+                try
+                {
+                    CorrelationId = new Correlation_Id(Text.Trim());
+                    return true;
+                }
+                catch (Exception)
+                { }
+            }
+
+            CorrelationId = default;
+            return false;
+
+        }
+
+        #endregion
+
+        #region Clone
+
+        /// <summary>
+        /// Clone this HTTP correlation identification.
+        /// </summary>
+        public Correlation_Id Clone
+
+            => new Correlation_Id(
+                   new String(InternalId?.ToCharArray())
+               );
+
+        #endregion
+
+
+        #region Operator overloading
+
+        #region Operator == (CorrelationId1, CorrelationId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="CorrelationId1">A HTTP correlation identification.</param>
+        /// <param name="CorrelationId2">Another HTTP correlation identification.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator == (Correlation_Id CorrelationId1,
+                                           Correlation_Id CorrelationId2)
+
+            => CorrelationId1.Equals(CorrelationId2);
+
+        #endregion
+
+        #region Operator != (CorrelationId1, CorrelationId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="CorrelationId1">A HTTP correlation identification.</param>
+        /// <param name="CorrelationId2">Another HTTP correlation identification.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator != (Correlation_Id CorrelationId1,
+                                           Correlation_Id CorrelationId2)
+
+            => !(CorrelationId1 == CorrelationId2);
+
+        #endregion
+
+        #region Operator <  (CorrelationId1, CorrelationId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="CorrelationId1">A HTTP correlation identification.</param>
+        /// <param name="CorrelationId2">Another HTTP correlation identification.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator < (Correlation_Id CorrelationId1,
+                                          Correlation_Id CorrelationId2)
+
+            => CorrelationId1.CompareTo(CorrelationId2) < 0;
+
+        #endregion
+
+        #region Operator <= (CorrelationId1, CorrelationId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="CorrelationId1">A HTTP correlation identification.</param>
+        /// <param name="CorrelationId2">Another HTTP correlation identification.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator <= (Correlation_Id CorrelationId1,
+                                           Correlation_Id CorrelationId2)
+
+            => !(CorrelationId1 > CorrelationId2);
+
+        #endregion
+
+        #region Operator >  (CorrelationId1, CorrelationId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="CorrelationId1">A HTTP correlation identification.</param>
+        /// <param name="CorrelationId2">Another HTTP correlation identification.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator > (Correlation_Id CorrelationId1,
+                                          Correlation_Id CorrelationId2)
+
+            => CorrelationId1.CompareTo(CorrelationId2) > 0;
+
+        #endregion
+
+        #region Operator >= (CorrelationId1, CorrelationId2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="CorrelationId1">A HTTP correlation identification.</param>
+        /// <param name="CorrelationId2">Another HTTP correlation identification.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator >= (Correlation_Id CorrelationId1,
+                                           Correlation_Id CorrelationId2)
+
+            => !(CorrelationId1 < CorrelationId2);
+
+        #endregion
+
+        #endregion
+
+        #region IComparable<CorrelationId> Members
+
+        #region CompareTo(Object)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Object">An object to compare with.</param>
+        public Int32 CompareTo(Object Object)
+
+            => Object is Correlation_Id locationId
+                   ? CompareTo(locationId)
+                   : throw new ArgumentException("The given object is not a correlation identification!",
+                                                 nameof(Object));
+
+        #endregion
+
+        #region CompareTo(CorrelationId)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="CorrelationId">An object to compare with.</param>
+        public Int32 CompareTo(Correlation_Id CorrelationId)
+
+            => String.Compare(InternalId,
+                              CorrelationId.InternalId,
+                              StringComparison.OrdinalIgnoreCase);
+
+        #endregion
+
+        #endregion
+
+        #region IEquatable<CorrelationId> Members
+
+        #region Equals(Object)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Object">An object to compare with.</param>
+        /// <returns>true|false</returns>
+        public override Boolean Equals(Object Object)
+
+            => Object is Correlation_Id locationId &&
+                   Equals(locationId);
+
+        #endregion
+
+        #region Equals(CorrelationId)
+
+        /// <summary>
+        /// Compares two HTTP correlation identifications for equality.
+        /// </summary>
+        /// <param name="CorrelationId">A HTTP correlation identification to compare with.</param>
+        /// <returns>True if both match; False otherwise.</returns>
+        public Boolean Equals(Correlation_Id CorrelationId)
+
+            => String.Equals(InternalId,
+                             CorrelationId.InternalId,
+                             StringComparison.OrdinalIgnoreCase);
+
+        #endregion
+
+        #endregion
+
+        #region GetHashCode()
+
+        /// <summary>
+        /// Return the HashCode of this object.
+        /// </summary>
+        /// <returns>The HashCode of this object.</returns>
+        public override Int32 GetHashCode()
+
+            => InternalId?.ToLower().GetHashCode() ?? 0;
+
+        #endregion
+
+        #region (override) ToString()
+
+        /// <summary>
+        /// Return a text representation of this object.
+        /// </summary>
+        public override String ToString()
+
+            => InternalId ?? "";
+
+        #endregion
+
+    }
+
+}
